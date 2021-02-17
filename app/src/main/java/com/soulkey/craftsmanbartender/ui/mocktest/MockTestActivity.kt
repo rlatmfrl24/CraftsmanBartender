01bbde452996cb7b6e7225ed2401c2a0d95fe3b5
김슬기 (Seul-Ki KIM) <2018159@clt.net>
2021-02-17 09:53:36 +0900

    This fixes #5

--- app/src/main/java/com/soulkey/craftsmanbartender/ui/mocktest/MockTestActivity.kt
+++ app/src/main/java/com/soulkey/craftsmanbartender/ui/mocktest/MockTestActivity.kt
@@ -1,17 +1,31 @@
 package com.soulkey.craftsmanbartender.ui.mocktest
 
 import android.os.Bundle
+import com.afollestad.materialdialogs.MaterialDialog
 import com.soulkey.craftsmanbartender.R
 import com.soulkey.craftsmanbartender.lib.common.BaseActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MockTestActivity : BaseActivity() {
 
+    private val mockTestViewModel : MockTestViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mock_test)
     }
 
     override fun onBackPressed() {
-        finish()
+        if (mockTestViewModel.isTestFinished.value == false) {
+            MaterialDialog(this)
+                    .title(text = "Warning")
+                    .message(text = "테스트가 완료되지 않았습니다. 정말 테스트를 종료할까요?")
+                    .positiveButton {
+                        finish()
+                    }
+                    .negativeButton()
+                    .show()
+        } else {
+            finish()
+        }
     }
 }
